25c11a0468032e307eff5b0f6ba319c9d1925b03
ytodojunto <ytodojunto@gmail.com>
2026-08-24 16:23:42 -0300

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
 // Pegar acá la URL de "Implementación" del Apps Script (termina en /exec)
 // Ejemplo: https://script.google.com/macros/s/AKfycb.../exec
-window.GUARDIA_API_URL = "https://script.google.com/macros/s/AKfycbyU6etxX7U1-RTiyGIVIuAcVsiaXkZahH6wXdupA0Aev3XO7D6shb5TP9wUZttPsllrKQ/exec";
+window.GUARDIA_API_URL = "https://script.google.com/macros/s/AKfycbwTMwOecSrHAbbJt89-M8j-TitZn2pfGur1KYaacMmMEt7eYyxwMEDcmhG4wUuWIKIy5w/exec";
